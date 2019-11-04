af752dfc549593fb70395abe69d4bea2c8040dd7
ElijahCoding <cuiyeqing960904@gmail.com>
2019-11-04 22:29:22 +0300

    sign in built

--- api/app/Http/Controllers/Auth/SignInController.php
+++ api/app/Http/Controllers/Auth/SignInController.php
@@ -9,6 +9,10 @@ class SignInController extends Controller
 {
     public function __invoke(Request $request)
     {
+        if (!$token = auth()->attempt($request->only('email', 'password'))) {
+            return response(null, 401);
+        }
 
+        return response()->json(compact('token'));
     }
 }
