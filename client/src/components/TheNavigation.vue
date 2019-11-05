8242c55d40dc0e357b95c33283b952508ccdacd2
ElijahCoding <cuiyeqing960904@gmail.com>
2019-11-05 08:22:09 +0300

    navbar

--- client/src/components/TheNavigation.vue
+++ client/src/components/TheNavigation.vue
@@ -1,5 +1,40 @@
 <template>
-    <div>
-        Nav
-    </div>
+    <ul>
+        <li>
+            <router-link
+                    :to="{
+                    name: 'home'
+                }"
+            >
+                Home
+            </router-link>
+        </li>
+            <li>
+                username
+            </li>
+
+            <li>
+                <router-link
+                        :to="{
+                            name: 'signin'
+                        }"
+                >
+                    Sign in
+                </router-link>
+            </li>
+            <li>
+                <router-link
+                        :to="{
+                            name: 'dashboard'
+                        }"
+                >
+                    Dashboard
+                </router-link>
+            </li>
+            <li>
+                <a href="#" @click.prevent="signOut">
+                    Sign out
+                </a>
+            </li>
+    </ul>
 </template>
