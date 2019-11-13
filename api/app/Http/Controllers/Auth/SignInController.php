a823bc83d1da6509a1bfe87b8f7de74f09f8299f
ElijahCoding <cuiyeqing960904@gmail.com>
2019-11-13 08:19:25 +0300

    prompting the user for a OPT

--- api/app/Http/Controllers/Auth/SignInController.php
+++ api/app/Http/Controllers/Auth/SignInController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Google2FA;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -10,7 +11,25 @@ class SignInController extends Controller
     public function __invoke(Request $request)
     {
         if (!$token = auth()->attempt($request->only('email', 'password'))) {
-            return response(null, 401);
+            return response([
+                'reason' => 'INCORRECT_CREDENTIALS'
+            ], 401);
+        }
+
+        $user = $request->user();
+
+        if ($user->google2fa_enabled && !$request->otp) {
+            return response([
+                'reason' => 'REQUEST_OTP'
+            ], 401);
+        }
+
+        if ($user->google2fa_enabled && $request->otp) {
+            if (!Google2FA::verifyKey($user->google2fa_secret, $request->otp)) {
+                return response([
+                    'reason' => 'INCORRECT_OTP'
+                ]);
+            }
         }
 
         return response()->json(compact('token'));
